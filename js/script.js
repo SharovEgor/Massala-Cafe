f07e280356d409ba0e90a5f2d1f7571bd0c91464
SharovEgor <sharov-egorka@mail.ru>
2019-05-04 19:27:16 +0300

    end section "feedback"

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#Feedback .Feedback__slider').slick({
         slidesToShow: 1,
         prevArrow: '#Feedback .Feedback__button-prev',
         nextArrow: '#Feedback .Feedback__button-next'
-});
+    });
 });
