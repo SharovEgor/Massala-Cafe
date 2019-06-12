44d040b860fde8b95190c6c378158fed04d4b99f
SharovEgor <sharov-egorka@mail.ru>
2019-06-13 01:56:15 +0300

    Add adaptive iframe

--- js/script.js
+++ js/script.js
@@ -35,14 +35,14 @@ window.addEventListener('load', function () {
         if (window.innerWidth < 1000) {
             map.style.transform = "translate(-1635px, -2328px)";
         }
-        if (window.innerWidth < 800) {
-            map.style.transform = "translate(-1635px, -2328px)";
+        if (window.innerWidth < 520) {
+            map.style.transform = "translate(-1785px, -2328px)";
         }
-        if (window.innerWidth < 400) {
+        /*if (window.innerWidth < 400) {
             map.style.transform = "translate(-1635px, -2328px)";
         } else {
             map.style.transform = "translate(-1635px, -2328px)";
-        }
+        }*/
     }
 
     adaptiveMap();
